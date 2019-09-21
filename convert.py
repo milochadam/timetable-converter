52be28ca86239db17b70e09e896ed11773b11c03
milochadam <miloch.adam@gmail.com>
2019-09-21 13:00:10 +0200

    Update

--- convert.py
+++ convert.py
@@ -1,136 +1,265 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import bs4
-import re
-import sys
+import copy
+from bs4 import BeautifulSoup, NavigableString
+from typing import List, Optional
 
-source = "SIS.html"
+# source = "SIS.html"
+source = "KAIMS.html"
+
 output = "out.html"
-if len(sys.argv) > 2:
-    source = str(sys.argv[1])
-    output = str(sys.argv[2])
+
+DAY_NAMES_PRESENT = True
+DAYS = 5
+HOURS = 15
+WEEKDAY_NAMES = [
+    'Poniedziałek',
+    'Wtorek',
+    'Środa',
+    'Czwartek',
+    'Piątek',
+]
+MERGE_CONSECUTIVE_COURSES = True
+
 
 def main():
-    # print("main")
-    soup = get_html_file(source)
-    parser = HTMLparser(soup)
-    parser.modify_soup()
-    save_html_file(parser.soup, output)
+    soup = get_soup_from_file(source)
+    parser = HTMLParser(soup)
+    parser.main()
 
 
-def get_html_file(filename):
-    with open(filename) as inf:
-        txt = inf.read()
-        soup = bs4.BeautifulSoup(txt, "html.parser")
-    return soup
+def get_soup_from_file(filename: str) -> BeautifulSoup:
+    with open(filename) as f:
+        html = f.read()
+        return BeautifulSoup(html, 'html.parser')
 
 
-def save_html_file(soup, filename):
-    with open(filename, "w") as outf:
-        outf.write(str(soup))
+def save_html_file(soup: BeautifulSoup, filename: str) -> None:
+    with open(filename, "w") as f:
+        f.write(str(soup))
 
 
-class HTMLparser():
-    def __init__(self, soup):
+class Course:
+    def __init__(self, dict_: Optional[dict] = None, empty=True):
+        self.empty = empty
+        self.room = None
+        self.type = None
+        self.name = None
+        self.teacher = None
+
+        self.hours = 1
+        self.checked = False
+        self.delete = False
+        if dict_:
+            for k, v in dict_.items():
+                setattr(self, k, v)
+
+    def __eq__(self, other: 'Course'):
+        return self.room == other.room and \
+               self.type == other.type and \
+               self.name == other.name and \
+               self.teacher == other.teacher
+
+    def to_html(self):
+        s = BeautifulSoup('', 'html.parser')
+        container = s.new_tag('div')
+        types = {
+            '[W]': 'lecture',
+            '[P]': 'project',
+            '[L]': 'laboratory',
+            '[C]': 'exercise',
+            '[S]': 'seminar',
+        }
+        room_tag = s.new_tag('div', attrs={'class': 'room'})
+        room_tag.string = self.room
+
+        # type
+        container['class'] = container.get('class', []) + [types[self.type]]
+
+        name_type_container = s.new_tag('div', attrs={'class': 'name-type-container'})
+        type_tag = s.new_tag('span', attrs={'class': 'type'})
+        type_tag.string = self.type
+        name_tag = s.new_tag('span', attrs={'class': 'name'})
+        name_tag.string = self.name
+        name_type_container.append(type_tag)
+        name_type_container.append(name_tag)
+
+        teacher_tag = s.new_tag('div', attrs={'class': 'teacher'})
+        teacher_tag.string = self.teacher
+
+        container.insert(0, room_tag)
+        # container.insert(1, type_tag)
+        container.insert(2, name_type_container)
+        container.insert(3, teacher_tag)
+        return container
+
+
+class HTMLParser:
+
+    def __init__(self, soup) -> None:
         self.soup = soup
 
-    def modify_soup(self):
-        self.remove_common_terms()
-        self.remove_clutter()
-        self.add_stylesheet()
-        self.add_classes()
-        self.add_js_stuff()
+    def main(self) -> None:
+        global DAY_NAMES_PRESENT
+        global HOURS
+        global DAYS
+        global MERGE_CONSECUTIVE_COURSES
+        new_plan = [[
+            [] for y in range(HOURS)]
+            for x in range(DAYS)]
+        s = self.soup
+        table_body = s.find(name='tbody')
 
-    def add_js_stuff(self):
-        self.soup.head.append(bs4.BeautifulSoup(
-            '<script type="text/javascript" src="scripts.js"></script>', "html.parser"))
-        self.soup.body['onload'] = 'enable_all()'
-        root = self.soup.find('div', {'class': 'blue_div'})
-        js_root = self.soup.new_tag('div')
-        root.append(js_root)
-        button_div = self.soup.new_tag('div')
-        button_div['class'] = 'button_div'
-        js_root.append(button_div)
+        # skip header
+        for y, row in enumerate(table_body.find_all(name='tr')[1:]):
+            # skip first cell with hour
+            for x, cell in enumerate(row.find_all(name='td')[1:]):
+                courses = self._string_to_courses(cell.children)
+                new_plan[x][y].extend(courses)
+                # print(courses)
 
-        self.add_button(button_div, 'toggle lectures',  'lecture')
-        self.add_button(button_div, 'toggle labs', 'lab')
-        self.add_button(button_div, 'toggle projects', 'project')
-        self.add_button(button_div, 'toggle exercises', 'exercise')
-        self.add_button(button_div, 'toggle last-10', 'last-10-weeks')
-        self.add_button(button_div, 'do 29.11/30.11', 'do-29-11')
+        if MERGE_CONSECUTIVE_COURSES:
+            for day in new_plan:
+                for y, hour in enumerate(day):
+                    if hour is []:
+                        continue
+                    for course in hour:
+                        if course.checked:
+                            course.delete = True
+                            continue
+                        next_hours = day[y+1:]
+                        for next_hour in next_hours:
+                            matching_course = [c for c in next_hour if course == c]
+                            if len(matching_course) == 1:
+                                matching_course[0].checked = True
+                                course.hours += 1
+                            else:
+                                break
 
-    def add_button(self, root, text, id_):
-        button = self.soup.new_tag('button')
-        button.append(text)
-        button['class'] = button.get('class', [])+['toggle_button']
-        button['id'] = id_
-        root.append(button)
+        new_soup = BeautifulSoup('<html></html>', 'html.parser')
+        new_soup.insert(0, s.new_tag('body'))
+        new_soup.insert(0, s.new_tag('head'))
+        timetable_div = s.new_tag('div')
+        timetable_div['class'] = timetable_div.get('class', []) + ['timetable-container']
+        new_soup.body.insert(0, timetable_div)
+        hours_div = s.new_tag('div')
+        for i in range(HOURS):
+            hour_div = s.new_tag('div')
+            hour_div['class'] = hour_div.get('class', []) + ['cell', 'hour']
+            hour_div.string = f'{i + 7}:00'
+            hours_div.append(hour_div)
+        if DAY_NAMES_PRESENT:
+            empty_cell = s.new_tag('div')
+            empty_cell['class'] = empty_cell.get('class', []) + ['cell', 'empty-in-corner']
+            hours_div.insert(0, empty_cell)
+        timetable_div.insert(0, hours_div)
+        for day_number, day in enumerate(new_plan):
+            day_div = s.new_tag('div')
+            day_div['class'] = day_div.get('class', []) + ['day']
+            if DAY_NAMES_PRESENT:
+                day_name_div = s.new_tag('div')
+                day_name_div.string = WEEKDAY_NAMES[day_number]
+                day_name_div['class'] = day_name_div.get('class', []) + ['day-name']
+                day_div.append(day_name_div)
 
-    def add_stylesheet(self):
-        self.soup.head.append(bs4.BeautifulSoup(
-            '<link rel="stylesheet" type="text/css" href="stylesheet.css">', "html.parser"))
+            for hour in day:
+                hour_div = s.new_tag('div')
+                hour_div['class'] = hour_div.get('class', []) + ['cell', 'course-container']
+                for i, course in enumerate(hour):
+                    course_div = course.to_html()
+                    course_div['class'] = course_div.get('class', []) + ['course']
+                    if len(hour) > 1:
+                        course_div['class'] = course_div.get('class', []) + ['half']
+                    if i == 0:
+                        course_div['class'] = course_div.get('class', []) + ['course-left']
+                    else:
+                        course_div['class'] = course_div.get('class', []) + ['course-right']
 
-    def add_classes(self):
-        self.add_class_for_text_grandparent('\[W\]', 'lecture')
-        self.add_class_for_text_grandparent('\[L\]', 'lab')
-        self.add_class_for_text_grandparent('\[P\]', 'project')
-        self.add_class_for_text_grandparent('\[C\]', 'exercise')
-        self.add_class_for_text_parent('[0-9]{1,2}:[0-9]{1,2}', 'hour')
-        self.add_class_for_text_grandparent('ostatnie', 'last-10-weeks')
-        self.add_class_for_text_grandparent('29|30\.', 'do-29-11')
+                    if MERGE_CONSECUTIVE_COURSES:
+                        if course.hours == 2:
+                            course_div['class'] = course_div.get('class', []) + ['double']
+                        if course.hours == 3:
+                            course_div['class'] = course_div.get('class', []) + ['triple']
+                        if course.delete:
+                            course_div['class'] = course_div.get('class', []) + ['deleted']
 
-    def add_class_for_text_grandparent(self, text, css_class):
-        for elem in self.soup(text=re.compile(text)):
-            parent = elem.parent.parent
-            parent['class'] = parent.get('class', [])+[css_class]
+                    hour_div.append(course_div)
+                if len(hour) == 0:
+                    hour_div['class'] = hour_div.get('class', []) + ['empty']
+                elif len(hour) > 0 and all([course.delete for course in hour]):
+                    hour_div['class'] = hour_div.get('class', []) + ['deleted']
 
-    def add_class_for_text_parent(self, text, css_class):
-        for elem in self.soup(text=re.compile(text)):
-            parent = elem.parent
-            parent['class'] = parent.get('class', [])+[css_class]
+                day_div.append(hour_div)
 
-    def remove_common_terms(self):
-        terminy_wspolne = self.soup.find_all('b', text=re.compile("termin"))
-        for term in terminy_wspolne:
-            self.remove_common_term(term)
+            new_soup.body.div.append(day_div)
+        new_soup.head.insert(0, BeautifulSoup('<link rel="stylesheet" href="stylesheet.css" type="text/css">', 'html.parser'))
+        save_html_file(new_soup, 'output.html')
 
-    def remove_common_term(self, endpoint):
-        current = endpoint
-        parent = current.parent
-        for _ in range(9):
-            current = self.remove_element(current)
+    @staticmethod
+    def _string_to_courses(tags) -> List[Course]:
+        courses = []
+        if tags is []:
+            return [Course(empty=True)]
 
-        if len(parent.contents) > 0:
-            child = parent.contents[0]
-            if child.name == 'br':
-                child.extract()
+        STATE_ROOM = 1
+        STATE_TYPE = 2
+        STATE_NAME = 3
+        STATE_TEACHER = 4
+        STATE_OTHER = 5
 
-    def remove_element(self, text):
-        """Removes element and returns previous sibling if exists"""
-        ret_value = text.previous_sibling
-        text.extract()
-        return ret_value
+        state = STATE_ROOM
 
-    def remove_clutter(self):
-        self.soup.html.attrs = None
-        self.remove_all_tags('script')
-        self.remove_all_tags('style')
-        self.remove_all_tags('link')
-        self.remove_all_tags('footer')
-        self.soup.find('div', class_="navbar").extract()
-        self.soup.find('div', class_="white_radius_div_center").extract()
-        self.soup.find('div', class_="center-div").extract()
-        for tag in self.soup.find_all(['th', 'tr', 'td'], attrs={'style': re.compile("background-color.*")}):
-            del tag['style']
-        for row in self.soup.find_all('tr'):
-            if len(row.text) < 51:
-                row.extract()
+        empty_course = {
+            'room': None,
+            'type': None,
+            'name': None,
+            'teacher': None,
+            'other': None,
+        }
+        course = empty_course
+        for tag in tags:
+            if tag.name == 'br':
+                continue
 
-    def remove_all_tags(self, tag_name):
-        result = self.soup.find_all(tag_name)
-        for res in result:
-            res.extract()
+            if isinstance(tag, NavigableString) and str(tag) == ' ':
+                continue
+
+            if state == STATE_ROOM:
+                course.update({'room': tag.get_text()})
+                state = STATE_TYPE
+                continue
+
+            if state == STATE_TYPE:
+                course.update({'type': tag.get_text()})
+                state = STATE_NAME
+                continue
+
+            if state == STATE_NAME:
+                course.update({'name': tag.get_text()})
+                state = STATE_TEACHER
+                continue
+
+            if state == STATE_TEACHER:
+                course.update({'teacher': str(tag.string)})
+                state = STATE_OTHER
+                continue
+
+            if state == STATE_OTHER:
+                course.update({'other': tag.get_text()})
+                courses.append(course)
+                course = copy.deepcopy(empty_course)
+                state = STATE_ROOM
+                continue
+
+            break
+
+        if state == STATE_OTHER:
+            courses.append(course)
+
+        courses = [Course(dict_=c, empty=False) for c in courses]
+
+        return courses
 
 
 if __name__ == "__main__":
